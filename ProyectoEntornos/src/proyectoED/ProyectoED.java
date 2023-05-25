def465426a79109c05deb05ba57c545d686b60ba
Ivan <ivanpelora2601@gmail.com>
2023-05-25 13:21:03 +0200

    Metodo umprimirDatosPersona creado

--- ProyectoEntornos/src/proyectoED/ProyectoED.java
+++ ProyectoEntornos/src/proyectoED/ProyectoED.java
@@ -1,8 +1,12 @@
 package proyectoED;
 
+import java.util.Scanner;
+
 public class ProyectoED {
 	/**
-	 * Metodo el cual utiliza un numero mínimo y otro máximo y devuelve un numero aleatorio entre esos dos números.
+	 * Metodo el cual utiliza un numero mínimo y otro máximo y devuelve un numero
+	 * aleatorio entre esos dos números.
+	 * 
 	 * @param Min
 	 * @param Max
 	 * @return Un numero aleatorio entre esos dos.
@@ -10,5 +14,14 @@ public class ProyectoED {
 	public int numeroRandom(int Min, int Max) {
 		return (int) (Math.random() * (Max - Min + 1) + Min);
 	}
-	
+	/**
+	 * Método el cual recibe una persona y dice su nombre y si es mayor o menor de edad.
+	 * @param persona
+	 * Devuelve su nombre y si es mayor o menor de edad
+	 */
+	public void imprimirDatosPersona(Persona persona) {
+
+		System.out.println("La persona se llama: " + persona.getNombre());
+		System.out.println("Es " + (persona.getEdad() > 18 ? "mayor " : "menor ") + "de edad");
+	}
 }
