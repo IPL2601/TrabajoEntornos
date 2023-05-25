9e2279eaf124ec92deb2af1ceb85a7909dc30599
Ivan <ivanpelora2601@gmail.com>
2023-05-25 13:10:01 +0200

    Metodo numero aleatorio finalizado

---
+++ ProyectoEntornos/src/proyectoED/ProyectoED.java
@@ -0,0 +1,16 @@
+package proyectoED;
+
+public class ProyectoED {
+	/**
+	 * Metodo el cual utiliza un numero mínimo y otro máximo y devuelve un numero aleatorio entre esos dos números.
+	 * @param Min
+	 * @param Max
+	 * @return Un numero aleatorio entre esos dos.
+	 */
+	public int numeroRandom(int Min, int Max) {
+		return (int) (Math.random() * (Max - Min + 1) + Min);
+	}
+	
+	
+
+}
